f79f0df6588684d74c479cdb0f1056beaa55dfef
Andreas Aronsson <aronsson@gmail.com>
2016-06-07 22:02:56 +0200

    autolayout programmaticaly

--- WorldTrotter/MapViewController.swift
+++ WorldTrotter/MapViewController.swift
@@ -7,12 +7,41 @@
 //
 
 import UIKit
+import MapKit
 
 class MapViewController: UIViewController {
     
+    var mapView: MKMapView!
+    
+    override func loadView() {
+        
+        mapView = MKMapView()
+        
+        view = mapView
+
+        
+        let segmentedControl = UISegmentedControl(items: ["Standard", "Hybrid", "Satellite"])
+        segmentedControl.backgroundColor = UIColor.whiteColor().colorWithAlphaComponent(0.5)
+        
+        segmentedControl.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(segmentedControl)
+        
+        let topConstraint = segmentedControl.topAnchor.constraintEqualToAnchor(view.topAnchor)
+        let leadingConstraint = segmentedControl.leadingAnchor.constraintEqualToAnchor(view.leadingAnchor)
+        let trailingConstraint = segmentedControl.trailingAnchor.constraintEqualToAnchor(view.trailingAnchor)
+        
+        topConstraint.active = true
+        leadingConstraint.active = true
+        trailingConstraint.active = true
+        
+        
+        
+        }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         print("MapViewController loaded its view")
     }
+    
+   
 }
