f1ca406fdcf026a10deefd36670bca73b5ac58e4
Andreas Aronsson <aronsson@gmail.com>
2016-06-07 13:34:49 +0200

    initial fahrenheit to celsius swift

---
+++ ConversionViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ConversionViewController.swift
+//  WorldTrotter
+//
+//  Created by Andreas Aronsson on 07/06/16.
+//  Copyright © 2016 Andreas Aronsson. All rights reserved.
+//
+
+import UIKit
+
+class ConversionViewController: UIViewController {
+    
+    @IBOutlet var celsiusLabel : UILabel!
+    @IBOutlet var textField : UITextField!
+    
+    var fahrenheitValue: Double?
+    
+    
+    var celsiusValue: Double? {
+        if let value = fahrenheitValue {
+           return (value - 32) * (5/9)
+        }
+        else {
+            return nil
+        }
+    }
+    
+    func updateCelsiusLabel() {
+        if let value = celsiusValue {
+            celsiusLabel.text = "\(value)"
+        } else {
+            celsiusLabel.text = "???"
+        }
+    }
+    
+    @IBAction func fahrenheitFieldEditingChanged(textField: UITextField) {
+        if let text = textField.text where !text.isEmpty {
+            celsiusLabel.text = text
+        }
+        else {
+            celsiusLabel.text = "???"
+        }
+    }
+    
+    @IBAction func dismissKeyboard(sender: AnyObject) {
+        textField.resignFirstResponder()
+    }
+    
+    
+}
